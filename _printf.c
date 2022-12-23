70dc6c8baee894cee3ff99db33f6de1223309971
Kenneth Attans <kennykayock@gmail.com>
2022-12-23 04:23:43 -0800

    Task _printf.c

--- _printf.c
+++ _printf.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
- *
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
+
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
@@ -39,19 +39,19 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
 	return (printed_chars);
 }
-
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
